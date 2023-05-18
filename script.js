36daea650e0f53da6c43fa2f2bd0561f3196461e
winstarboy <19uca001@loyolacollege.edu>
2023-05-18 18:34:30 +0530

    neumorphic changes

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-const inputBox = document.getElementById("input-box")
-const listContainer = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addButton = document.getElementById("add-button");
 
 const addTask = () => {
-    if(inputBox.value === '') {
-        alert("You must write something!")
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-    else {
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.value
-        listContainer.appendChild(li)
-        let span = document.createElement("span")
-        span.innerHTML= "\u00d7"
-        li.appendChild(span)
+    inputBox.value = '';
+    saveData();
+};
+
+const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+        addTask();
     }
-    inputBox.value = ''
-    saveData()
-}
+};
+
+addButton.addEventListener("click", addTask);
+inputBox.addEventListener("keypress", handleKeyPress);
 
 listContainer.addEventListener("click", function(e) {
-    if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("checked")
-        saveData()
-    }
-    else if(e.target.tagName === "SPAN") {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveData()
+        saveData();
     }
-}, false)
+}, false);
 
-const saveData = () => localStorage.setItem("data", listContainer.innerHTML)
+const saveData = () => localStorage.setItem("data", listContainer.innerHTML);
 
+const showTask = () => listContainer.innerHTML = localStorage.getItem("data");
 
-const showTask = () => listContainer.innerHTML = localStorage.getItem("data")
-
-showTask()
+showTask();
